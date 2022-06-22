3221774edb622d6e700cf0496cf699f07848b104
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-06-22 10:34:31 -0500

    completed function practice through #4

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,23 +14,78 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
+
+// Added the desired return: saying hello and referencing the name parameter.
+
 function helloName( name ) {
-  return;
-}
+  console.log('in helloName:', name);
+  return "Hello " + name + '!';
+} // end helloName
+
 // Remember to call the function to test
 
+console.log('helloName test:', helloName('Lons'));
+console.log('helloName test:', helloName('Alice'));
+console.log('helloName test:', helloName('Corinne'));
+console.log('helloName test:', helloName(45834));
+console.log('helloName test:', helloName(true));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+
+// Added a second parameter name because this function needs to take two arugments.
+// Created a variable named 'sum' which is assigned the value of firstNumber + secondNumber.
+// Created a conditional to check if both entered parameters are numbers.
+// If both parameters are numbers, the function returns the variable 'sum'.
+// If one or both parameters are not numbers, the function returns "Enter numbers only, please."
+
+function addNumbers( firstNumber, secondNumber ) {
+  console.log('in addNumbers:', firstNumber, secondNumber);
+  let sum = firstNumber + secondNumber;
+  if(typeof firstNumber === 'number' && typeof secondNumber === 'number'){
+    // return firstNumber + secondNumber;
+    return sum;
+  } else{
+    return 'Enter numbers only, please.';
+  }
 }
 
+// function test
+
+console.log('addNumbers test:', addNumbers(4, 7));
+console.log('addNumbers test:', addNumbers(-4, 7));
+console.log('addNumbers test:', addNumbers(-3, -23));
+console.log('addNumbers test:', addNumbers(1000, 45000));
+console.log('addNumbers test:', addNumbers(0, 0.12678375));
+console.log('addNumbers test:', addNumbers(true, 4));
+console.log('addNumbers test:', addNumbers('it is a string', 87));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
 
+// Added three parameter names within the function ().
+// Created variable named 'product' which is assigned the value of num1 * num2 * num3.
+// Created a conditional to check if all three entered parameters are numbers.
+// If all parameters are a number, the function returns the variable 'product'.
+// If any parameters are not a number, the function returns "Enter numbers only, please."
+
+function multiplyThree(num1, num2, num3){
+  console.log('in multiplyThree:', num1, num2, num3);
+  let product = num1 * num2 * num3;
+  if(typeof num1 === 'number' && typeof num2 === 'number' && typeof num3 === 'number'){
+    // return num1 * num2 * num3
+    return product;
+  } else{
+    return 'Enter numbers only, please.';
+  }
 }
 
+// function test
+
+console.log('multiplyThree test:', multiplyThree(1, 2, 3));
+console.log('multiplyThree test:', multiplyThree(5, 10, 10));
+console.log('multiplyThree test:', multiplyThree(true, 1, 11));
+console.log('multiplyThree test:', multiplyThree(-1, 2, 3));
+console.log('multiplyThree test:', multiplyThree(0.25, 2, 3.5));
+console.log('multiplyThree test:', multiplyThree('string', 100, 3));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
